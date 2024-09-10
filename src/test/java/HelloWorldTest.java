6f66593af6a5d4d5c4605c8a7baa750e4e2f4afc
Andrey <Andrej_3@mail.ru>
2024-09-10 21:44:06 +0300

    Ex5: Парсинг JSON

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,15 +1,30 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
+    public void testRestAssured(){
+        /*
+        Map<String, String> params = new HashMap<>();
+        params.put("message", "And this is a second message");
+        params.put("timestamp", "2021-06-04 16:41:51");
+         */
+
+        JsonPath response = RestAssured
+                .given()
+                //.queryParams(params)
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
         response.prettyPrint();
+
+        String answer = response.get("messages[1].\"message\"");
+        System.out.println(answer);
     }
 
 }
