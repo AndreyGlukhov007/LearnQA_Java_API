0eb1fd025e38c9ff5cf72834cdcee0eb335bcd9a
Andrey <Andrej_3@mail.ru>
2024-09-11 17:42:48 +0300

    Ex6: Редирект

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,24 +1,22 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured(){
-        JsonPath response = RestAssured
+        Response response = RestAssured
                 .given()
-                //.queryParams(params)
-                .get("https://playground.learnqa.ru/api/get_json_homework")
-                .jsonPath();
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
         response.prettyPrint();
 
-        String answer = response.get("messages[1].\"message\"");
-        System.out.println(answer);
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
     }
 
 }
