fd153954b761f12e6645c9c3fefbee8d9352a66d
Andrey <Andrej_3@mail.ru>
2024-09-29 15:09:40 +0300

    Ex9: Подбор пароля

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,62 +1,126 @@
+
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import java.lang.Thread;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.*;
 
 public class HelloWorldTest {
 
     @Test
-    public void testRestAssured() throws InterruptedException {
-            JsonPath responseOne = RestAssured
-                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
-                    .jsonPath();
+    public void testRestAssured() {
 
-            //responseOne.prettyPrint(); - этот метод можно удалить. Он нужен был для отлаживания.
+        List<String> allPassword = new ArrayList<>();
+        allPassword.add("password");
+        allPassword.add("123456");
+        allPassword.add("12345678");
+        allPassword.add("qwerty");
+        allPassword.add("abc123");
+        allPassword.add("monkey");
+        allPassword.add("1234567");
+        allPassword.add("letmein");
+        allPassword.add("trustno1");
+        allPassword.add("dragon");
+        allPassword.add("baseball");
+        allPassword.add("111111");
+        allPassword.add("iloveyou");
+        allPassword.add("master");
+        allPassword.add("sunshine");
+        allPassword.add("ashley");
+        allPassword.add("bailey");
+        allPassword.add("passw0rd");
+        allPassword.add("shadow");
+        allPassword.add("123123");
+        allPassword.add("654321");
+        allPassword.add("superman");
+        allPassword.add("qazwsx");
+        allPassword.add("michael");
+        allPassword.add("Football");
+        allPassword.add("12345");
+        allPassword.add("qwerty123");
+        allPassword.add("1q2w3e4r");
+        allPassword.add("admin");
+        allPassword.add("qwertyuiop");
+        allPassword.add("555555");
+        allPassword.add("lovely");
+        allPassword.add("7777777");
+        allPassword.add("welcome");
+        allPassword.add("888888");
+        allPassword.add("princess");
+        allPassword.add("password1");
+        allPassword.add("123qwe");
+        allPassword.add("666666");
+        allPassword.add("football");
+        allPassword.add("!@#$%^&*");
+        allPassword.add("charlie");
+        allPassword.add("aa123456");
+        allPassword.add("donald");
+        allPassword.add("donald");
+        allPassword.add("login");
+        allPassword.add("starwars");
+        allPassword.add("hello");
+        allPassword.add("freedom");
+        allPassword.add("whatever");
+        allPassword.add("1234567890");
+        allPassword.add("1234");
+        allPassword.add("solo");
+        allPassword.add("121212");
+        allPassword.add("flower");
+        allPassword.add("hottie");
+        allPassword.add("loveme");
+        allPassword.add("zaq1zaq1");
+        allPassword.add("zaq1zaq1");
+        allPassword.add("1qaz2wsx");
+        allPassword.add("mustang");
+        allPassword.add("access");
+        allPassword.add("696969");
+        allPassword.add("batman");
+        allPassword.add("adobe123");
+        allPassword.add("photoshop");
+        allPassword.add("azerty");
+        allPassword.add("000000");
+        allPassword.add("jesus");
+        allPassword.add("ninja");
 
-            String token = responseOne.get("token");
-            int time = responseOne.get("seconds");
+        for (int i = 0; i < allPassword.size(); i++){
 
-            JsonPath responseTwo = RestAssured
-                    .given()
-                    .queryParam("token",token)
-                    .when()
-                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
-                    .jsonPath();
+            Map<String, String> date =  new HashMap<>();
+            date.put("login", "super_admin");
+            date.put("password", allPassword.get(i));
 
-            //responseTwo.prettyPrint(); - этот метод можно удалить. Он нужен был для отлаживания.
-
-            String statusResponseOne = responseTwo.get("status");
-            if (statusResponseOne.equals("Job is NOT ready")){
-                System.out.println("Отлично. Метод API продолжает работать. Ждём когда завершится метод.");
-            } else {
-                System.out.println("Что-то пошло не так. Метод будет завершён");
-                return;
-            }
-
-            Thread.sleep(time*1000); //Мы получаем из метода ответ в секундах. А функция sleep() отрабатывает в миллисекундах.
-            //таким образом через умножение на 1000 мы переводим секунды в миллисекунды.
-
-            JsonPath responseThree = RestAssured
+            Response responseForSecretPassword = RestAssured
                 .given()
-                .queryParam("token",token)
+                .body(date)
                 .when()
-                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
-                .jsonPath();
+                .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                .andReturn();
 
-            //responseThree.prettyPrint(); - этот метод можно удалить. Он нужен был для отлаживания.
+            String cookieForSecretPassword = responseForSecretPassword.getCookie("auth_cookie");
 
-            String statusResponseTwo = responseThree.get("status");
-            String result = responseThree.get("result");
-            if (statusResponseTwo.equals("Job is ready") & result != null){
-                System.out.println("Отлично. Тестирование метода завершено.");
-            } else {
-                System.out.println("Что-то пошло не так. Метод будет завершён");
-                return;
-            }
+            Map<String, String> cookieForCheckAuth =  new HashMap<>();
+            cookieForCheckAuth.put("auth_cookie", cookieForSecretPassword);
 
-            //responseThree.prettyPrint(); - этот метод можно удалить. Он нужен был для отлаживания.
+            Response responseForAuthorization = RestAssured
+                .given()
+                .cookies(cookieForCheckAuth)
+                .when()
+                .post("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                .andReturn();
+
+            String responseText = responseForAuthorization.print();
+            if(responseText.equals("You are authorized")){
+                System.out.println("Фраза из ответ " + responseText);
+                System.out.println("Пароль " + allPassword.get(i));
+                break;
+            };
+
+        }
 
     }
 
